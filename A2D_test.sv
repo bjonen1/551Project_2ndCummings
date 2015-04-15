5f11aaf5ec572fd7bd485d17cc96dc8a0d7c493e
Brandon Lee Jonen <bjonen@tux-3.cae.wisc.edu>
2015-04-15 11:33:36 -0500

    added correct test_demo

--- A2D_test.sv
+++ A2D_test.sv
@@ -1,43 +1,87 @@
-module A2D_test(clk,RST_n,nxt_chnnl,LEDs,a2d_SS_n,MOSI,MISO,SCLK);
+module A2D_test(clk, RST_n, nxt_chnnl, LEDs, a2d_SS_n, MOSI, MISO, SCLK);
+	input RST_n, nxt_chnnl, clk, MISO;
+	output [7:0] LEDs;
+	output MOSI, SCLK, a2d_SS_n;
+	wire rst_n;
+	wire button_rise_edge;
+	wire strt_cnv;
+	wire cnv_cmplt;
+	wire [11:0] res;
 
-input clk,RST_n;		// 50MHz clock and active low unsynchronized reset from push button
-input nxt_chnnl;		// unsynchronized push button.  Advances to convert next chnnl
-output [7:0] LEDs;		// upper bits of conversion displayed on LEDs
-output a2d_SS_n;		// Active low slave select to A2D (part of SPI bus)
-output MOSI;			// Master Out Slave In to A2D (part of SPI bus)
-input MISO;				// Master In Slave Out from A2D (part of SPI bus)
-output SCLK;			// Serial clock of SPI bus
+//next_byte_logic
+reg next_byte_reg1, next_byte_reg2, next_byte_reg3;
+always@(negedge clk or negedge rst_n)begin
+	if(~rst_n)
+		next_byte_reg1 <= 1'b1;
+	else
+		next_byte_reg1 <= nxt_chnnl;
+end
 
-///////////////////////////////////////////////////
-// Declare any registers or wires you need here //
-/////////////////////////////////////////////////
-wire [11:0] res;		// result of A2D conversion
+always@(negedge clk or negedge rst_n)begin
+	if(~rst_n)
+		next_byte_reg2 <= 1'b1;
+	else
+		next_byte_reg2 <= next_byte_reg1;
+end
 
-/////////////////////////////////////
-// Instantiate Reset synchronizer //
-///////////////////////////////////
-reset_synch iRST(.clk(clk), .RST_n(RST_n), .rst_n(rst_n));
+always@(negedge clk or negedge rst_n)begin
+	if(~rst_n)
+		next_byte_reg3 <= 1'b1;
+	else
+		next_byte_reg3 <= next_byte_reg2;
+end
 
-////////////////////////////////
-// Instantiate A2D Interface //
-//////////////////////////////
-A2D_intf iA2D(.clk(clk), .rst_n(rst_n), .strt_cnv(strt_cnv), .cnv_cmplt(), .chnnl(chnnl),
-              .res(res), .a2d_SS_n(a2d_SS_n), .SCLK(SCLK), .MOSI(MOSI), .MISO(MISO));
+assign button_rise_edge = ((~next_byte_reg3) & next_byte_reg2);
 
 
-////////////////////////////////////////
-// Synchronize nxt_chnnl push button //
-//////////////////////////////////////
- 
+//////RESET LOGIC
 
-///////////////////////////////////////////////////////////////////
-// Implement method to increment channel and start a conversion //
-// with every release of the nxt_chnnl push button.            //
-////////////////////////////////////////////////////////////////
-	
-	
-	
+reg reset_flop1;
+reg reset_flop2;
+
+always @(negedge clk or negedge RST_n) begin
+	if (!RST_n)
+		reset_flop1 <= 1'b0;
+	else
+		reset_flop1 <= 1'b1;
+end
+
+always @(negedge clk or negedge RST_n) begin
+	if (!RST_n)
+		reset_flop2 <= 1'b0;
+	else
+		reset_flop2 <= reset_flop1;
+end
+
+assign rst_n = reset_flop2;
+
+
+////DriveA2DLogic
+reg [2:0] chnnl;
+reg button_rise_to_start_cnv; //need a reg to make the trigger from button rise sync with the channel
+
+always@(posedge clk or negedge rst_n)begin
+	if(!rst_n)
+		chnnl <= 3'b000;
+	else if (button_rise_edge & (chnnl == 3'b110))
+		chnnl <= 3'b000;
+	else if (button_rise_edge)
+		chnnl <= chnnl + 1;
+	else
+		chnnl <= chnnl;
+end
+
+always@(posedge clk or negedge rst_n)begin
+	if(!rst_n)
+		button_rise_to_start_cnv <= 1'b0;
+	else
+		button_rise_to_start_cnv <= button_rise_edge;
+end
+assign strt_cnv = button_rise_to_start_cnv;
+
+
+
+A2D_intf A2D_one(clk,rst_n,strt_cnv,cnv_cmplt,chnnl,res,a2d_SS_n,SCLK,MOSI,MISO);
 assign LEDs = res[11:4];
 
 endmodule
-    
